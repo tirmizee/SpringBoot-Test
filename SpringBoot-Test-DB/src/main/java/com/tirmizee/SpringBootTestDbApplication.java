f3537d462f7eb5cb5ec64c4a49f24d57480b2c87
Pratya Yeekhaday <zee_pratya@hotmail.com>
2021-08-10 23:12:14 +0700

    init SpringBoot-Test-DB project

---
+++ SpringBoot-Test-DB/src/main/java/com/tirmizee/SpringBootTestDbApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestDbApplication.class, args);
+	}
+
+}
